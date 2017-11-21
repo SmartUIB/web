d003c058d5fef1eb1d58fc3aad6a04da319ee923
Pablo <pablo.riutort@gmail.com>
2017-11-21 13:01:19 +0100

    Add models, migration and admin
    Create models for:
    * Idea
    * Challenge
    * New
    
    Create migration file for new models
    Register models in admin site

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,37 @@
+# encoding=utf8
 from django.db import models
+from django.core.validators import URLValidator
 
-# Create your models here.
+
+class New(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    date = models.DateField() 
+    url = models.TextField(validators=[URLValidator()])
+    media = models.ImageField()
+    
+    def __str__(self):
+        return "{0} ({1})".format(self.title, self.date)
+
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
+
+
+class Idea(models.Model):
+    title = models.CharField(max_length=128)
+    challenge = models.ForeignKey(Challenge, related_name="idea_challenge", null=True)
+    description = models.TextField()
+    date = models.DateField() 
+    public = models.BooleanField()
+    email = models.EmailField(null=True)
+    
+    def __str__(self):
+        idea_str = "Idea by {0}".format(self.email)
+        if self.title and self.challenge:
+            idea_str += " ({0})".format(self.challenge)
+        idea_str += " at {0}".format(self.date)
+        return idea_str
