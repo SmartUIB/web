255163dd1f46644be59aca17212de4c487dad3fe
Pablo <pablo.riutort@gmail.com>
2017-11-22 18:56:11 +0100

    Add templates and homepage page
    Add static images, main.js and main.css
    Add STATIC_ROOT in settings.py
    Add templates folder with index.html
    
    Set the view for index view in views.py, link with url in urls.py

---
+++ homepage/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+
+from homepage import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from homepage.models import New, Idea
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'news_list': New.objects.order_by('date'),
+        'ideas_list': Idea.objects.order_by('date'),
+    }
+    
+    return render(request, 'index.html', context)
