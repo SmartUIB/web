9613ce2d46d4934632df2b99ffb843d29bb73be2
Pablo <pablo.riutort@gmail.com>
2018-03-22 13:05:14 +0100

    Heroku uploading v2
    Following next tutorial:
    https://simpleisbetterthancomplex.com/tutorial/2016/08/09/how-to-deploy-django-applications-on-heroku.html

--- smart/settings.py
+++ smart/settings.py
@@ -123,21 +123,19 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-ENV_PATH = os.path.abspath(os.path.dirname(__file__))
-# Media files
-MEDIA_ROOT = 'media/'
-MEDIA_URL = '/media/'
-
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+ENV_PATH = os.path.abspath(os.path.dirname(__file__))
+# Media files
+MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
 
 
 # Simplified static file serving.
